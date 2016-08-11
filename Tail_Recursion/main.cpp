b7a65ded19938fa01f9af745bbdc1d9fbe12c2e2
Ferenc Szalma <szalmaf@users.noreply.github.com>
2016-08-11 16:51:59 -0400

    Initial Commit

---
+++ Tail_Recursion/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Tail_Recursion
+//
+//  Created by Ferenc Szalma on 8/11/16.
+//  Copyright © 2016 Ferenc Szalma. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
